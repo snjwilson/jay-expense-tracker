92b1fb05ef3a5460187e67508efd5267f0abd41f
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2024-11-03 12:47:15 +0530

    Modify user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
