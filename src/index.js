0665e9378896563bb9f1597b2aac8f5627710ebe
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2024-11-03 13:08:29 +0530

    Use bodyparser middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { startDB } = require("./initDB");
 const { router } = require("./routes/apiv1");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 8000;
 
 startDB().then(() => {
+  // middlewares
+  app.use(bodyParser.json());
   app.use(router);
 
+  // listen and serve
   app.listen(port, () => {
     console.log(`Expense app listening on port ${port}`);
   });
