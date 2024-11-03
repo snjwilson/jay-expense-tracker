d8535f5eed2d70ffb69c5e48be3df7cf187c71e4
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2024-11-03 13:08:16 +0530

    Update API v1

--- src/routes/apiv1.js
+++ src/routes/apiv1.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { userRouter } = require("./user");
-const { expenseRouter } = require("./expense");
+const userRouter = require("./user");
+const expenseRouter = require("./expense");
+const categoryRouter = require("./category");
 
 const router = express.Router();
 
 router.use("/users", userRouter);
-
 router.use("/expenses", expenseRouter);
+router.use("/categories", categoryRouter);
 
 module.exports = { router };
